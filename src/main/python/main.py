34499bc970322198c1c01817faf3573a6418cfe2
AINDUSTRIES <alexandredaoust4@gmail.com>
2021-11-03 18:12:24 +0100

    Launch done

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -25,7 +25,7 @@ class Startup(QtWidgets.QWidget):
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setStyleSheet("QFrame {background-color: red;}")
 
-        self.setStyleSheet("QLabel {color: red;}")
+        self.setStyleSheet("QLabel {color: red}")
 
         self.g_layout.addWidget(self.lbl_aind)
         self.g_layout.addWidget(self.line)
@@ -36,25 +36,33 @@ class Startup(QtWidgets.QWidget):
         self.lineanim_size = QtCore.QPropertyAnimation(self.line, b"size")
         self.lineanim_size.setEasingCurve(QtCore.QEasingCurve.InOutCubic)
         self.lineanim_size.setStartValue(QtCore.QSize(0, 3))
-        self.lineanim_size.setEndValue(QtCore.QSize(522, 3))
+        self.lineanim_size.setEndValue(QtCore.QSize(538, 3))
         self.lineanim_size.setDuration(1000)
+
+        self.lineanim_pos = QtCore.QPropertyAnimation(self.line, b"pos")
+        self.lineanim_pos.setEasingCurve(QtCore.QEasingCurve.InOutCubic)
+        self.lineanim_pos.setStartValue(QtCore.QPoint(670, 370))
+        self.lineanim_pos.setEndValue(QtCore.QPoint(415, 370))
+        self.lineanim_pos.setDuration(1000)
 
         self.posanim = QtCore.QPropertyAnimation(self.lbl_pres, b"pos")
         self.posanim.setEasingCurve(QtCore.QEasingCurve.InOutCubic)
-        self.posanim.setStartValue(QtCore.QPoint(self.lbl_pres.x(),self.lbl_pres.y() - 25))
-        self.posanim.setEndValue(QtCore.QPoint(self.lbl_pres.x(),self.lbl_pres.y()))
+        self.posanim.setStartValue(QtCore.QPoint(415,370))
+        self.posanim.setEndValue(QtCore.QPoint(415,390))
         self.posanim.setDuration(1000)
 
         self.sizeanim = QtCore.QPropertyAnimation(self.lbl_pres, b"size")
         self.sizeanim.setEasingCurve(QtCore.QEasingCurve.InOutCubic)
-        self.sizeanim.setStartValue(QtCore.QSize(self.lbl_pres.width(), 0))
-        self.sizeanim.setEndValue(QtCore.QSize(self.lbl_pres.width(), self.lbl_pres.height()))
-        self.sizeanim.setDuration(2000)
+        self.sizeanim.setStartValue(QtCore.QSize(537, 0))
+        self.sizeanim.setEndValue(QtCore.QSize(537, 60))
+        self.sizeanim.setDuration(1000)
 
         self.animgroup.addAnimation(self.lineanim_size)
-        #self.animgroup.addAnimation(self.posanim)
+        self.animgroup.addAnimation(self.posanim)
         self.animgroup.addAnimation(self.sizeanim)
+        self.animgroup.addAnimation(self.lineanim_pos)
         self.animgroup.start()
+
 
 
 if __name__ == '__main__':
